768a6418a015865742bfde4e92e3f902748ef136
Prachi <75129191+PrachiSGodbole@users.noreply.github.com>
2021-03-22 09:27:13 +0530

    Basic security config

--- src/main/java/com/schools/management/Controller/MainController.java
+++ src/main/java/com/schools/management/Controller/MainController.java
@@ -1,6 +1,9 @@
 package com.schools.management.Controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,16 +11,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
 
-	/*
-	 * @RequestMapping(value={"/home"},method=RequestMethod.GET) public ModelAndView
-	 * home() { ModelAndView model = new ModelAndView(); model.setViewName("index");
-	 * return model; }
-	 */
-	
 	@RequestMapping(value={"/home"},method=RequestMethod.GET)    
-	public String home1()  
+	public ModelAndView home()   
 	{
-	   return "index";
+		ModelAndView model = new ModelAndView();
+		model.setViewName("login");
+		return model;
 	}
 	
 	@RequestMapping(value={"/login"},method={RequestMethod.GET})    
@@ -50,5 +49,11 @@ public class MainController {
 		ModelAndView model = new ModelAndView();
 		model.setViewName("contact");
 		return model;
+	}
+	
+	@GetMapping("/signout")
+	public String destroySession(HttpServletRequest request) {
+		request.getSession().invalidate();
+		return "redirect:/login";
 	}
 }
